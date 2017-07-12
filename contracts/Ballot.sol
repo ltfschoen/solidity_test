563c8d358b195de505e39e5c7f140994661c581b
Luke Schoen <ltfschoen@gmail.com>
2017-07-13 08:32:10 +1000

    feat: Add extra functions to Ballot contract

--- contracts/Ballot.sol
+++ contracts/Ballot.sol
@@ -2,11 +2,12 @@ pragma solidity ^0.4.11;
 
 /// @title Voting with delegation
 contract Ballot {
+
     /// Declare to represent Single Voter
     struct Voter {
         uint weight; // weight accumulated by delegation
         bool voted;  // whether person already voted
-        address delegate; // person delegated to
+        address delegate; // person delegated to (Delegatee)
         uint vote;   // index of voted proposal
     }
 
@@ -18,6 +19,12 @@ contract Ballot {
 
     address public chairperson;
 
+    bytes32 public value;
+
+    address[] votersCollection;
+
+    bool public initiated;
+
     /// Declare state variable to store `Voter` struct for each possible address.
 
     // Iterate mapping: https://forum.ethereum.org/discussion/1995/iterating-mapping-types
@@ -26,8 +33,8 @@ contract Ballot {
     /// Dynamically-sized array of `Proposal` structs.
     Proposal[] public proposals;
 
-    /// Create new Ballot to with:
-    //  - chairperson (originator) grante voting authority by changing `weight` to `1`
+    /// Constructor creates new Ballot to with:
+    //  - chairperson (originator) granted voting authority by changing `weight` to `1`
     //  - proposals (comprising list of proposalNames/candidates) each with voteCount
     function Ballot(bytes32[] proposalNames) {
         chairperson = msg.sender;
@@ -36,10 +43,14 @@ contract Ballot {
         // Iterate `proposalNames`, create New `Proposal` Object, append to `proposals` array
         for (uint i = 0; i < proposalNames.length; i++) {
             proposals.push(Proposal({
-            name: proposalNames[i],
-            voteCount: 0
+                name: proposalNames[i],
+                voteCount: 0
             }));
         }
+
+        votersCollection.push(chairperson);
+
+        initiated = true;
     }
 
     /// Grant Voter authority to vote on Ballot through call only from chairperson
@@ -49,7 +60,13 @@ contract Ballot {
     function giveRightToVote(address voter) {
         // `require` - http://solidity.readthedocs.io/en/develop/control-structures.html
         require((msg.sender == chairperson) && !voters[voter].voted && (voters[voter].weight == 0));
+        // if (msg.sender != chairperson || voters[voter].voted) {
+        //   // `throw` terminates and reverts all changes to state and Ether balances but consume all provided Gas
+        //   throw;
+        // }
         voters[voter].weight = 1;
+
+        votersCollection.push(voter);
     }
 
     /// Delegate Vote to voter with `to` address
@@ -61,6 +78,13 @@ contract Ballot {
 
         require(!sender.voted);
         require(to != msg.sender);
+
+        // if (sender.voted)
+        //   throw;
+
+        // if (to == msg.sender) {
+        //   throw;
+        // }
 
         // // WARNING: Loops are dangerous since they may cause the Contract
         // //   to get stuck or if they run too long more Gas than is available
@@ -84,12 +108,12 @@ contract Ballot {
         // - `voted` property set `true` since defers vote to `delegate`
         // - `delegate` property set to `to` address
         //
-        // Delegate `to` address passed by reference to `delegate` (of Voter type) then
+        // Delegatee `to` address passed by reference to `delegate` (of Voter type) then
         // - If `delegate` has already voted then add `voteCount` of the `Proposal` in list
         //   of `proposals` with the `weight` (extent of voting influence) value of the Delegator
         // - Else if `delegate` has not voted yet then assign the Delegator `sender` voting `weight`
         //   to the `delegate` for use when they proceed to vote on behalf of the Delegator
-        //   (the Delegate may have a large `weight` if multiple Delegators deferred their `weight` to
+        //   (the Delegatee may have a large `weight` if multiple Delegators deferred their `weight` to
         //   allowing them to vote multiple times)
 
         // Modify `voters[msg.sender].voted` since `sender` passed by reference
@@ -117,8 +141,8 @@ contract Ballot {
         // (to avoid throwing later and having to revert all changes)
 
         // TODO: If more than one Delegators (i.e. 2 voters) delegate their
-        //   voting weight to a specific Delegate voter, then as soon as that
-        //   Delegate votes, then their `voted` property is set `true` and then ALL of their
+        //   voting weight to a specific Delegatee voter, then as soon as that
+        //   Delegatee votes, then their `voted` property is set `true` and then ALL of their
         //   available weight is transferred to a SINGLE `proposal`, which
         //   prevents them from spreading their available weight (voting power)
         //   across multiple proposals that they may wish to support
@@ -140,8 +164,33 @@ contract Ballot {
         }
     }
 
-    // Calls winningProposal() to get index of winner from `proposals` array then returns `winnerName`
+    /// Calls winningProposal() to get index of winner from `proposals` array then returns `winnerName`
     function winnerName() constant returns (bytes32 winnerName) {
         winnerName = proposals[winningProposal()].name;
+    }
+
+    /// Get count of `proposals` dynamic array of structs
+    function proposalsCount() constant returns (uint) {
+        return proposals.length;
+    }
+
+    /// Get specific properties of `Proposal` at index in `proposals`
+    function getProposalName(uint index) constant returns (bytes32 proposalName) {
+        return proposals[index].name;
+    }
+
+    function getProposalVoteCount(uint index) constant returns (uint voteCount) {
+        return proposals[index].voteCount;
+    }
+
+    /// Get count of `voters`
+    function votersCount() constant returns (uint votersCount) {
+        return votersCollection.length;
+    }
+
+    function getVoter(uint index) constant returns (address, uint, bool, address, uint) {
+        address voterAddress = votersCollection[index];
+        Voter voter = voters[voterAddress];
+        return (voterAddress, voter.weight, voter.voted, voter.delegate, voter.vote);
     }
 }
