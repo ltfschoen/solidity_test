ac26c8cc449e5c1a270ac6cc6f70208ce90d5153
Luke Schoen <ltfschoen@gmail.com>
2017-07-13 08:32:34 +1000

    feat: Change to synchronose migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,10 @@
 const SimpleStorage = artifacts.require("./SimpleStorage.sol");
 const SubCurrency = artifacts.require("./SubCurrency.sol");
+const Ballot = artifacts.require("./Ballot.sol");
 
+// http://truffle.readthedocs.io/en/beta/getting_started/migrations/
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage)
-    .then(function() {
-      return deployer.deploy(SubCurrency);
-    });
+  deployer.deploy(SimpleStorage);
+  deployer.deploy(SubCurrency);
+  deployer.deploy(Ballot);
 };
