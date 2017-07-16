3008c400866b19439c3fc3b04459711e00223455
Luke Schoen <ltfschoen@gmail.com>
2017-07-17 08:21:11 +1000

    feat: Add Simple Open Auction to Migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,12 @@
 const SimpleStorage = artifacts.require("./SimpleStorage.sol");
 const SubCurrency = artifacts.require("./SubCurrency.sol");
 const Ballot = artifacts.require("./Ballot.sol");
+const SimpleOpenAuction = artifacts.require("./SimpleOpenAuction.sol");
 
 // http://truffle.readthedocs.io/en/beta/getting_started/migrations/
 module.exports = function(deployer) {
   deployer.deploy(SimpleStorage);
   deployer.deploy(SubCurrency);
   deployer.deploy(Ballot);
+  deployer.deploy(SimpleOpenAuction);
 };
